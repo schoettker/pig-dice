3f5f84c3b8b23e07a23a22f5e8bfa1b1f10aa5d4
Schoettker <lennartschoettker@hotmail.com>
2016-08-05 12:55:16 +0200

    change player when dice result is a 1

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,14 @@ Player.prototype.information = function () {
   $("#score").text(this.score);
   $("#turn-total").text(this.turnTotal);
 };
-function changePlayer(activePlayer){
-  if (activePlayer === player1){
-    currentPlayer = player2;
-  } else {
-    currentPlayer = player1;
-  }
-};
-var player1;
-var player2;
+
 // user interface logic
 $(document).ready(function(){
   $("#play").click(function(){
     var name = prompt("Enter a name for Player 1");
-    player1 = new Player(name, 0, 0);
+    var player1 = new Player(name, 0, 0);
     name = prompt("Enter a name for Player 2");
-    player2 = new Player(name, 0, 0);
+    var player2 = new Player(name, 0, 0);
     var currentPlayer = player1;
     $("#play").slideUp();
     currentPlayer.information();
@@ -35,7 +27,9 @@ $(document).ready(function(){
       var result = rollDice();
       if (result === 1){
         currentPlayer.turnTotal = 0;
-        currentPlayer = player2;
+        if (currentPlayer === player1){
+          currentPlayer = player2;
+        } else { currentPlayer = player1;}
       } else {
         currentPlayer.turnTotal += result;
       }
