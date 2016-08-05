665e4ad0cb6ccc3315337bfd19e5d0cdbd31eb22
Schoettker <lennartschoettker@hotmail.com>
2016-08-05 12:52:00 +0200

    player can roll the dice and result gets displayed, player change still needs to get to work

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,43 @@ function Player(name,turnTotal,score){
   this.turnTotal = turnTotal;
   this.score = score;
 }
-Player.prototype.rollDice = function () {
+var rollDice = function () {
   return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 };
 Player.prototype.information = function () {
   $("#name").text(this.name);
   $("#score").text(this.score);
-  $("#turn-total").text(this.score);
+  $("#turn-total").text(this.turnTotal);
 };
-
+function changePlayer(activePlayer){
+  if (activePlayer === player1){
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
+};
+var player1;
+var player2;
 // user interface logic
 $(document).ready(function(){
   $("#play").click(function(){
     var name = prompt("Enter a name for Player 1");
-    var player1 = new Player(name, 0, 0);
+    player1 = new Player(name, 0, 0);
     name = prompt("Enter a name for Player 2");
-    var player2 = new Player(name, 0, 0);
+    player2 = new Player(name, 0, 0);
+    var currentPlayer = player1;
     $("#play").slideUp();
-    player1.information();
-    
+    currentPlayer.information();
+    $("#roll").click(function(){
+      var result = rollDice();
+      if (result === 1){
+        currentPlayer.turnTotal = 0;
+        currentPlayer = player2;
+      } else {
+        currentPlayer.turnTotal += result;
+      }
+      $("#roll-result").text(result);
+      currentPlayer.information();
+    });
   });
 });
