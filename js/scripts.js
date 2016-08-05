51b8eeff9ec85eec08f2e4dd6f2887ac6244e196
Schoettker <lennartschoettker@hotmail.com>
2016-08-05 12:36:40 +0200

    add information method to display current playerstats

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,11 @@ function Player(name,turnTotal,score){
 Player.prototype.rollDice = function () {
   return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 };
-
+Player.prototype.information = function () {
+  $("#name").text(this.name);
+  $("#score").text(this.score);
+  $("#turn-total").text(this.score);
+};
 
 // user interface logic
 $(document).ready(function(){
@@ -17,7 +21,7 @@ $(document).ready(function(){
     name = prompt("Enter a name for Player 2");
     var player2 = new Player(name, 0, 0);
     $("#play").slideUp();
+    player1.information();
+    
   });
-
-
 });
