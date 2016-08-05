9d940f6ed16f43472f6e653171447778d8909155
Schoettker <lennartschoettker@hotmail.com>
2016-08-05 12:02:22 +0200

    prompt User for playernames when play is clicked, create Player Objects and hide play button

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ function Player(name,turnTotal,score){
 Player.prototype.rollDice = function () {
   return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 };
+
+
+// user interface logic
+$(document).ready(function(){
+  $("#play").click(function(){
+    var name = prompt("Enter a name for Player 1");
+    var player1 = new Player(name, 0, 0);
+    name = prompt("Enter a name for Player 2");
+    var player2 = new Player(name, 0, 0);
+    $("#play").slideUp();
+  });
+
+
+});
